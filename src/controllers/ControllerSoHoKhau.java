966529a8f0501891c355544ac089d8523d85e8ee
nhduong1203 <nhduong1203@gmail.com>
2022-01-07 23:22:14 +0700

    Them chuc nang tra cuu ho khau, nhan khau

--- src/controllers/ControllerSoHoKhau.java
+++ src/controllers/ControllerSoHoKhau.java
@@ -11,13 +11,14 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import models.ModelSoHoKhau;
+import models.ModelNhanKhau;
 
 /**
  *
  * @author HO ANH
  */
 public class ControllerSoHoKhau {
-        public ArrayList<ModelSoHoKhau> selectDanhMucSoHoKhau () throws SQLException, ClassNotFoundException{
+    public ArrayList<ModelSoHoKhau> selectDanhMucSoHoKhau () throws SQLException, ClassNotFoundException{
         ArrayList<ModelSoHoKhau> resultList = new ArrayList<ModelSoHoKhau>();
 
         String sql = "SELECT * FROM ho_khau";
@@ -28,11 +29,48 @@ public class ControllerSoHoKhau {
         ){
             ResultSet rs = pstmt.executeQuery();
             while(rs.next()){
-                ModelSoHoKhau newMTT = new ModelSoHoKhau(rs.getString(1), rs.getString(3), rs.getString(4));
+                ModelSoHoKhau newMTT = new ModelSoHoKhau(rs.getString("hoVaTenChuHo"), rs.getString("ngayDangKi"), rs.getString("diaChi"));
                 resultList.add(newMTT);
             }
             return resultList;
         }
+    }
+
+    public ModelSoHoKhau TraCuuSHK(String SoSHK) throws SQLException, ClassNotFoundException{
+        String sql = "Select * from ho_khau where soHoKhau = ?";
+        ModelSoHoKhau newSHK = new ModelSoHoKhau();
+        try (
+            Connection con = ConnectDatabase.openConnection();
+            PreparedStatement pstmt = con.prepareStatement(sql);
+        ){
+            pstmt.setString(1, SoSHK);
+            ResultSet rs = pstmt.executeQuery();
+            if(rs.next()){
+                newSHK.setHoVaTenChuHo(rs.getString("hoVaTenChuHo"));
+                newSHK.setSoHoKhau(rs.getString("soHoKhau"));
+                newSHK.setDiaChi(rs.getString("diaChi"));
+                newSHK.setNgayDangKi(rs.getString("ngayDangKi"));
+                newSHK.setNguoiTao(rs.getString("nguoiTao"));
+            }
+            return newSHK;
         }
+    }
+
+    public ArrayList<ModelNhanKhau> getListNhanKhau_fromSoSHK(String SoSHK) throws SQLException, ClassNotFoundException{
+        String sql = "Select * from nhan_khau where soHoKhau = ?";
+        ArrayList<ModelNhanKhau> resultList = new ArrayList<ModelNhanKhau>();
+        try (
+            Connection con = ConnectDatabase.openConnection();
+            PreparedStatement pstmt = con.prepareStatement(sql);
+        ){
+            pstmt.setString(1, SoSHK);
+            ResultSet rs = pstmt.executeQuery();
+            while(rs.next()){
+                ModelNhanKhau newMTT = new ModelNhanKhau(rs.getString("hoVaTen"), rs.getString("ngaySinh"), rs.getInt("gioiTinh"), rs.getString("quanHeChuHo"), rs.getString("noiOHienTai"));
+                resultList.add(newMTT);
+            }
+            return resultList;
+        }
+    }
 
 }
--- src/views/form/FormDanhMucTamVang.java
+++ src/views/form/FormDanhMucTamVang.java
@@ -66,7 +66,7 @@ public class FormDanhMucTamVang extends javax.swing.JPanel {
 
         jLabel2.setFont(new java.awt.Font("SansSerif", 1, 12)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(51, 51, 255));
-        jLabel2.setText("Danh Mục Tạm Vắng");
+        jLabel2.setText("DANH MỤC TẠM VẮNG");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
